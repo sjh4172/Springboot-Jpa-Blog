10aadd319a272518e9d2b0bb4cf81fba0bdfb267
junghoon <hoon4172@gmail.com>
2024-10-28 23:38:13 +0900

    게시글 수정 기능 추가

---
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -0,0 +1,60 @@
+package com.cos.blog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.blog.model.Board;
+import com.cos.blog.model.RoleType;
+import com.cos.blog.model.User;
+import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.repository.UserRepository;
+
+
+@Service	// 스프링이 컴포넌트 스캔을 통해 Bean 등록 해줌
+public class BoardService {
+	@Autowired
+	private BoardRepository boardRepository;
+
+	@Transactional(readOnly = true)
+	public Page<Board> 글목록(Pageable pageable){
+		return boardRepository.findAll(pageable);
+	}
+
+	@Transactional(readOnly = true)
+	public Board 글상세보기(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을수 없습니다.");
+				});
+	}
+
+	@Transactional
+	public void 글삭제하기(int id) {
+		boardRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void 글쓰기(Board board, User user) {
+		board.setCount(0);
+		board.setUser(user);
+		boardRepository.save(board);
+	}
+	
+	@Transactional
+	public void 글수정하기(int id, Board requestBoard) {
+		Board board = boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 찾기 실패: 아이디를 찾을수 없습니다.");
+				});
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+		//해당 함수 종료시 트랜잭션이 종료됨, 이때 더티체킹 - 자동 update
+		
+	}
+}
